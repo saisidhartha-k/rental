088cf3defe2f6edf11793ccbd721dfc014308f67
saisidharth-k <saisidhartha.k@prodapt.com>
2023-08-31 17:31:06 +0530

    final commit

--- src/main/java/com/cycle/rental/repository/BorrowedCyclesRepository.java
+++ src/main/java/com/cycle/rental/repository/BorrowedCyclesRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BorrowedCyclesRepository extends JpaRepository<BorrowedCycles, Integer> {
-    // You can add custom query methods here if needed
 }
